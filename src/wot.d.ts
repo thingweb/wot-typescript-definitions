264ac4dc7f7cde27e350c92345c800a1c7a387d4
Johannes Hund (h0ru5) <johannes.hund@gmail.com>
2016-08-30 09:05:51 +0200

    added definitions for WoT and consumedthing

--- src/wot.d.ts
+++ src/wot.d.ts
@@ -3,21 +3,31 @@
 import {Promise} from 'es6-promise';
 
 interface WoTFactory {
-    consumeDescriptionUri(uri: String): Promise<ConsumedThing>
-    createThing(name: String): Promise<ExposedThing>
-    createFromDescriptionUri(uri: String): Promise<ExposedThing>
+    discover(discoveryType : string, filter : Object)
+    consumeDescriptionUri(uri: string): Promise<ConsumedThing>
+    consumeDescription(thingDescription : Object): Promise<ConsumedThing>
+    createThing(name: string): Promise<ExposedThing>
+    createFromDescriptionUri(uri: string): Promise<ExposedThing>
+    createFromDescription(thingDescription : Object): Promise<ExposedThing>
 }
 
 interface ConsumedThing {
-
+    name : string
+    invokeAction(actionName : string, parameter : any) : Promise<any>
+    setProperty(propertyName : string, newValue : any) : Promise<any>
+    getProperty(propertyName : string) : Promise<any>
+    addListener(eventName : string, listener : (event : Event) => void) : ConsumedThing
+    removeListener(eventName : string, listener : (event : Event) => void) : ConsumedThing
+    removeAllListeners(eventName : string) : ConsumedThing
+    getDescription() : Object 
 }
 
 interface ExposedThing {
-
+//onInvokeAction etc.
 }
 
 interface DynamicThing {
-
+//addproperty etc.
 }
 
 declare var WoT : WoTFactory;
